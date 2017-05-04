b9346b62212174b9840f86de8797177771a298c4
Joseph <joseph@codecreations.net>
2017-05-04 16:49:56 -0400

    adding more line breaks between prompts

--- prompts.js
+++ prompts.js
@@ -1,5 +1,6 @@
 module.exports = {
   call: function (i, inquirer, questions, cb) {
+    console.log("\n\n");
     inquirer.prompt(questions[i]).then(function (data) {
       cb(data.value);
     });
